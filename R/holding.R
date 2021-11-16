1aa0c53f263797b92bc76ea45bac0e1b4cdc8289
Ben Guinaudueau <bguinaudeau@gmail.com>
2021-11-16 20:35:35 +0100

    fix new version

--- R/holding.R
+++ R/holding.R
@@ -4,15 +4,25 @@ read_holding <- function(url){
   tmp <- tempfile()
 
   res <- httr::GET(url,
-                    httr::add_headers(`user-agent` = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36 Edg/90.0.818.62"))
+                   httr::add_headers(`user-agent` = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36 Edg/90.0.818.62"),
+                   httr::timeout(2)
+  )
   writeBin(res$content, tmp)
 
   tmp %>%
-    tabulizer::extract_tables(method = "stream") %>%
-    purrr::map_dfr(tibble::as_tibble) %>%
-    purrr::set_names(head(t(as.matrix(.[1,])))) %>%
-    # purrr::set_names(c("rank", head(t(as.matrix(.[1,])), -1))) %>%
-    tail(-1) %>%
+    tabulizer::extract_tables(method = "stream") %>% #bashR::simule_map(1)
+    purrr::map_dfr(~{
+      tmp <- tibble::as_tibble(.x) %>%
+        dplyr::filter(V2 != "") %>%
+        dplyr::glimpse() %>%
+        purrr::set_names(t(as.matrix(.[1,]))) %>%
+        tail(-1)
+
+      if(ncol(tmp) == 6){
+        names(tmp)[1] <- "Company"
+      }
+      return(tmp)
+    }) %>%
     janitor::clean_names() %>%
     dplyr::mutate_all(numerize) %>%
     dplyr::mutate(stamp = lubridate::now(tzone = "CET"))
@@ -24,14 +34,14 @@ get_holding <- function(etf = c("innovation", "genomic", "fintech", "space",
                                 "israel", "internet", "robot", "printing")){
   url <- switch(
     etf,
-    "innovation" = "https://ark-funds.com/wp-content/fundsiteliterature/holdings/ARK_INNOVATION_ETF_ARKK_HOLDINGS.pdf",
-    "genomic" = "https://ark-funds.com/wp-content/fundsiteliterature/holdings/ARK_GENOMIC_REVOLUTION_MULTISECTOR_ETF_ARKG_HOLDINGS.pdf",
-    "fintech" = "https://ark-funds.com/wp-content/fundsiteliterature/holdings/ARK_FINTECH_INNOVATION_ETF_ARKF_HOLDINGS.pdf",
-    "space" = "https://ark-funds.com/wp-content/fundsiteliterature/holdings/ARK_SPACE_EXPLORATION_&_INNOVATION_ETF_ARKX_HOLDINGS.pdf",
-    "israel" = "https://ark-funds.com/wp-content/fundsiteliterature/holdings/ARK_ISRAEL_INNOVATIVE_TECHNOLOGY_ETF_IZRL_HOLDINGS.pdf",
-    "internet" = "https://ark-funds.com/wp-content/fundsiteliterature/holdings/ARK_NEXT_GENERATION_INTERNET_ETF_ARKW_HOLDINGS.pdf",
-    "robot" = "https://ark-funds.com/wp-content/fundsiteliterature/holdings/ARK_AUTONOMOUS_TECHNOLOGY_&_ROBOTICS_ETF_ARKQ_HOLDINGS.pdf",
-    "printing" = "https://ark-funds.com/wp-content/fundsiteliterature/holdings/THE_3D_PRINTING_ETF_PRNT_HOLDINGS.pdf"
+    "innovation" = "https://ark-funds.com/wp-content/uploads/funds-etf-pdf/ARK_INNOVATION_ETF_ARKK_HOLDINGS.pdf",
+    "genomic" = "https://ark-funds.com/wp-content/uploads/funds-etf-pdf/ARK_GENOMIC_REVOLUTION_ETF_ARKG_HOLDINGS.pdf",
+    "fintech" = "https://ark-funds.com/wp-content/uploads/funds-etf-pdf/ARK_FINTECH_INNOVATION_ETF_ARKF_HOLDINGS.pdf",
+    "space" = "https://ark-funds.com/wp-content/uploads/funds-etf-pdf/ARK_SPACE_EXPLORATION_&_INNOVATION_ETF_ARKX_HOLDINGS.pdf",
+    "israel" = "https://ark-funds.com/wp-content/uploads/funds-etf-pdf/ARK_ISRAEL_INNOVATIVE_TECHNOLOGY_ETF_IZRL_HOLDINGS.pdf",
+    "internet" = "https://ark-funds.com/wp-content/uploads/funds-etf-pdf/ARK_NEXT_GENERATION_INTERNET_ETF_ARKW_HOLDINGS.pdf",
+    "robot" = "https://ark-funds.com/wp-content/uploads/funds-etf-pdf/ARK_AUTONOMOUS_TECH._&_ROBOTICS_ETF_ARKQ_HOLDINGS.pdf",
+    "printing" = "https://ark-funds.com/wp-content/uploads/funds-etf-pdf/THE_3D_PRINTING_ETF_PRNT_HOLDINGS.pdf"
     # Crypto/Mobility
   )
   url %>%
@@ -42,7 +52,7 @@ get_holding <- function(etf = c("innovation", "genomic", "fintech", "space",
 #' numerize
 #' @export
 numerize <- function(x){
-  if(!any(stringr::str_detect(x, "[^\\,\\.\\d]"))){
+  if(!any(stringr::str_detect(purrr::discard(x, is.na), "[^\\,\\.\\d]"))){
     as.numeric(stringr::str_remove_all(x, "\\,"))
   } else {
     x
